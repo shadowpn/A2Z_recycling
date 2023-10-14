57a7c24afdc241183458a3a032f351d18b2826da
NATALIIA PETRYCHUK <shadowpn@gmail.com>
2023-10-14 18:03:30 +0300

    Page About us

--- www/js/main.js
+++ www/js/main.js
@@ -31,4 +31,42 @@
     // Вызываем функцию также при изменении размера окна (для адаптивности)
     window.addEventListener('resize', setEqualCardHeight);
 
+    $(document).ready(function () {
+      // Найти все элементы .simpleCardsContent_content
+      var contentBlocks = $(".simpleCardsContent_content");
 
+      // Инициализировать переменную для хранения максимальной высоты
+      var maxHeight = 0;
+
+      // Перебрать каждый элемент .simpleCardsContent_content и найти максимальную высоту
+      contentBlocks.each(function () {
+        var currentHeight = $(this).outerHeight(); // Получить текущую высоту блока
+        if (currentHeight > maxHeight) {
+          maxHeight = currentHeight; // Обновить максимальную высоту, если текущая высота больше
+        }
+      });
+
+      // Установить максимальную высоту для всех элементов .simpleCardsContent_content
+      contentBlocks.css("height", maxHeight + "px");
+    });
+
+/*масштабировать фотографии Trust */
+window.addEventListener("load", function () {
+  const photoRow = document.getElementById("photo-row");
+  const images = photoRow.querySelectorAll("img");
+
+  let maxWidth = 0;
+
+  // Находим максимальную ширину фотографии в ряду
+  images.forEach(function (img) {
+    const imgWidth = img.width;
+    if (imgWidth > maxWidth) {
+      maxWidth = imgWidth;
+    }
+  });
+
+  // Устанавливаем максимальную ширину для всех фотографий
+  images.forEach(function (img) {
+    img.style.width = maxWidth + "px";
+  });
+});
